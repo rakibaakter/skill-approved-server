b1aedda834c44aa1d9fa97f5e99989be570d8188
rakibaakter <rakibaakter635@gmail.com>
2023-11-08 18:01:09 +0600

    created delete api

--- index.js
+++ index.js
@@ -93,6 +93,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/postedJob/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await postedJobCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
